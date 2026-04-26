ec411a7ef7a9da6fe0a3b0d4a4de0a56415ceb96
Pablo <Pablogonzalez1515@gmail.com>
2026-04-26 15:30:43 -0400

    Añadida foto de Triple Margarita

--- supabase/005_promos.sql
+++ supabase/005_promos.sql
@@ -2,7 +2,7 @@
 DELETE FROM products WHERE category_id = 'promos';
 
 INSERT INTO products (category_id, name, description, price, is_available, image_url, base_ingredients) VALUES
-('promos', 'Triple Margarita', '3 Margaritas Pequeñas, Salsa y Queso Mozarella', 10.00, true, '/images/pizzas/margarita_personal.png', ARRAY['Salsa de tomate', 'Mozzarella']),
+('promos', 'Triple Margarita', '3 Margaritas Pequeñas, Salsa y Queso Mozarella', 10.00, true, '/images/pizzas/triple_margarita_promo.png', ARRAY['Salsa de tomate', 'Mozzarella']),
 ('promos', 'Doble Jamón', '2 Jamón y Queso Pequeñas, Salsa, Queso Mozarella y Jamón + Refresco de Litro 1/2', 10.00, true, '/images/pizzas/jamon_y_queso_personal.png', ARRAY['Salsa de tomate', 'Mozzarella', 'Jamón']),
 ('promos', 'Dúo Sabor', '2 Pizzas Medianas (1 de Tocineta y 1 de Pepperoni) + Refresco de Litro 1/2', 20.00, true, '/images/pizzas/pepperoni_medium.png', ARRAY['Salsa de tomate', 'Mozzarella', 'Tocineta', 'Pepperoni']),
 ('promos', 'Margarita Doble Sabor', 'Familiar Rectangular: Salsa, Queso Mozarella + 2 INGREDIENTES DE SU PREFERENCIA + 1 Refresco de Litro 1/2', 24.00, true, '/images/pizzas/margarita_doble_sabor_promo.png', ARRAY['Salsa de tomate', 'Mozzarella']),
